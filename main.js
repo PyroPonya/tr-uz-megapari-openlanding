795d6d128d1004cf6770b505d8824f382431d0fe
Chetyrkin Alex <pyroponya@gmail.com>
2022-12-13 15:17:17 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,82 @@
+const langSelector = document.querySelector('.lang_selector');
+const langPicker = document.querySelector('.lang_picker');
+const langList = document.querySelector('.lang_list');
+const langListElements = document.querySelectorAll('.list_el');
+
+let currentLanguage = '';
+// lang setter
+const getPrefLang = () => {
+  let userLang = navigator.language || navigator.userLanguage;
+  if (userLang.includes('uzb')) {
+    currentLanguage = 'uzb';
+  } else if (userLang.includes('tr')) {
+    currentLanguage = 'tr';
+  } else {
+    currentLanguage = 'uzb';
+  }
+}
+getPrefLang();
+// lang end
+// mobile adaptation
+let isMobile = false;
+const checkMobile = () => {
+  window.innerWidth < 500 ? isMobile = true : isMobile = false;
+}
+checkMobile();
+window.onresize = (event) => {
+  checkMobile();
+  setLang(currentLanguage);
+};
+// 
+const langs = {
+  uzb: {
+    body: '100% birinchi depozit bonusini oling!',
+    btn: 'BONUS OLISH',
+    bg: './assets/bg_uzb_desk.jpg',
+    bg_mob: './assets/bg_uzb_mob.jpg',
+  },
+  tr: {
+    body: '%100 ilk para yatırma bonusunu alın!',
+    btn: 'IKRAMIYE ALMAK',
+    bg: './assets/bg_tr_desk.jpg',
+    bg_mob: './assets/bg_tr_mob.jpg',
+  },
+};
+// set qwerty
+const setLang = (lang) => {
+  const el = document.createElement('div');
+  el.innerHTML = `
+    <div class="el_flag"></div>
+    <div class="el_name">${currentLanguage.toUpperCase()}</div>`;
+  el.className = 'list_el';
+  el.className += ` ${currentLanguage}`;
+  langPicker.innerHTML = '';
+  langPicker.append(el);
+  document.querySelector('.body_text').textContent = langs[lang].body;
+  document.querySelector('.body_btn').textContent = langs[lang].btn;
+  if (isMobile == false) {
+    document.getElementsByTagName('body')[0].style.backgroundImage = `url('${langs[lang].bg}')`;
+  } else {
+    document.getElementsByTagName('body')[0].style.backgroundImage = `url('${langs[lang].bg_mob}')`;
+
+  }
+  location.hash = currentLanguage;
+}
+
+// lang list open/close
+langSelector.addEventListener('click', (e) => {
+  document.querySelector('.lang_list').classList.toggle('hide');
+});
+// lang list select item (slojnaya hernya tipa)
+langListElements.forEach(el => el.addEventListener('click', (e) => {
+  if ( e.target.closest('.list_el').classList.contains('uzb')) {
+    currentLanguage = 'uzb';
+    setLang(currentLanguage);
+  } else if (e.target.closest('.list_el').classList.contains('tr')) {
+    currentLanguage = 'tr';
+    setLang(currentLanguage);
+  }
+}))
+
+// initial lang
+setLang(currentLanguage);
